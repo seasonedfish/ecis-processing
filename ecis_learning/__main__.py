49e306b922ef7d5be0149ba17596e8ab4c917d74
Fisher Sun <fisher521.fs@gmail.com>
2020-07-11 14:15:48 -0400

    Separate data into columns

--- ecis_learning/__main__.py
+++ ecis_learning/__main__.py
@@ -4,4 +4,10 @@ file_location: str = "STC_dx.json"
 
 df = pd.read_json(file_location)
 
+# Separate data into columns
+df = df.join(df["diagnosis"].apply(pd.Series))
+df = df.drop(columns=["diagnosis", "P_MRN_ID", "E_ID"])
+df["DIAG_DATE"] = pd.to_datetime(df["DIAG_DATE"])
+
 print(df)
+print(df.info(verbose=True))
