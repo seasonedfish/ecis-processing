9ed2c9ab7de81272f0361f072ad1865562718efd
Fisher Sun <fisher521.fs@gmail.com>
2020-07-11 15:44:25 -0400

    Get earliest year and latest year

--- ecis_learning/__main__.py
+++ ecis_learning/__main__.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 file_location: str = "STC_dx.json"
 
-df = pd.read_json(file_location)
+df: pd.DataFrame = pd.read_json(file_location)
 
 # Separate data into columns
 df = df.join(df["diagnosis"].apply(pd.Series))
 df = df.drop(columns=["diagnosis", "P_MRN_ID", "E_ID"])
 df["DIAG_DATE"] = pd.to_datetime(df["DIAG_DATE"])
 
-print(df)
-print(df.info(verbose=True))
+# Get earliest year and latest year
+earliest_year = int(min(df.DIAG_DATE).strftime("%Y"))
+latest_year = int(max(df.DIAG_DATE).strftime("%Y"))
+
+
