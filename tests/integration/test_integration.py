f95733c958a2b3b60718cb2710c4b9dcaae0f22c
Fisher Sun <fisher521.fs@gmail.com>
2020-08-10 13:44:40 -0400

    Add initial integration test

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,13 @@
+import filecmp
+import os
+import pytest
+
+
+@pytest.fixture()
+def stc_ehr_expected_output() -> str:
+    return "tests/integration/fixtures/STC_EHR/processed.csv"
+
+
+def test_integration(tmp_path, stc_ehr_expected_output):
+    os.system(f"python main.py tests/integration/fixtures/STC_EHR/ -o {tmp_path}/processed.csv")
+    assert filecmp.cmp(f"{tmp_path}/processed.csv", stc_ehr_expected_output)
