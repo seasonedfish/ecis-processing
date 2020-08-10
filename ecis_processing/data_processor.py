bcc083e4330795f107dfa2fbc75295f525ef674a
Fisher Sun <fisher521.fs@gmail.com>
2020-08-10 16:13:49 -0400

    Rename field "Data" to "data"

--- ecis_processing/data_processor.py
+++ ecis_processing/data_processor.py
@@ -7,7 +7,7 @@ class DataProcessor:
     Class that processes medical data.
     """
     def __init__(self, df):
-        self.Data = df
+        self.data = df
 
     def get_processed_data(self, columns: List[str], suffix: str = "") -> pd.DataFrame:
         """
@@ -18,13 +18,13 @@ class DataProcessor:
         """
         # Create rows for new DataFrame, one for each patient
         rows: List[list] = []
-        patient_ids: List[str] = self.Data["patient_id"].unique()
+        patient_ids: List[str] = self.data["patient_id"].unique()
         for patient_id in patient_ids:
             rows.append([patient_id])
 
         # Get earliest year and latest year
-        earliest_year: int = int(min(self.Data["year"]))
-        latest_year: int = int(max(self.Data["year"]))
+        earliest_year: int = int(min(self.data["year"]))
+        latest_year: int = int(max(self.data["year"]))
 
         # Iterate through year intervals and add data to rows
         # Also get names of columns
@@ -56,7 +56,7 @@ class DataProcessor:
         :param columns: which columns to get values from
         """
         for row in rows:
-            interval_df: pd.DataFrame = self.Data.query(query)
+            interval_df: pd.DataFrame = self.data.query(query)
 
             if interval_df.empty:
                 row.append(None)
