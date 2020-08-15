fc670df918b921b2d9f15aa4d1a16ff83f41dce5
Fisher Sun <fisher521.fs@gmail.com>
2020-08-14 22:40:15 -0400

    Update get_processed_data()

--- ecis_processing/data_processor.py
+++ ecis_processing/data_processor.py
@@ -17,32 +17,12 @@ class DataProcessor:
         :param columns: which columns to get values from
         :param suffix: string that is appended to the end of year ranges
         """
-        return (self.data.groupby(["patient_id", pd.Grouper(freq="2Y", key="date")])
-                .agg(lambda column: "#".join(column))
-                .sum()
-                .unstack(fill_value=""))
-
-    @staticmethod
-    def combine(df):
-        df["value"] = "#".join([df["dx_code"], df["dx_name"]])
-        return df
-
-    def append_values(self, rows: List[list], query: str, columns: List[str]) -> None:
-        """
-        Queries the data with a boolean expression and appends the column values to every row.
-
-        :param rows: rows to append to
-        :param query: boolean expression to query the data
-        :param columns: which columns to get values from
-        """
-        for row in rows:
-            interval_df: pd.DataFrame = self.data.query(query)
-
-            if interval_df.empty:
-                row.append(None)
-            else:
-                result = (
-                    "#".join(map(str, values))
-                    for values in zip(*(interval_df[column] for column in columns))
-                )
-                row.append("; ".join(result))
+        df_new = self.data.copy()
+        df_new["data"] = self.data[columns].apply(lambda row: "#".join(row.values.astype(str)), axis=1)
+        df_new = (df_new.groupby(["patient_id", pd.Grouper(freq="2Y", key="date")])
+                  ["data"]
+                  .apply("; ".join)
+                  .unstack(fill_value=""))
+        df_new.rename(columns=lambda date: f"{date.year}-{date.year + 1}_{suffix}", inplace=True)
+        print(df_new)
+        return df_new
