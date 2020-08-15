cad54ec4adec881e4d753e512df85eb4d0431265
Fisher Sun <fisher521.fs@gmail.com>
2020-08-15 00:03:27 -0400

    Add NaT columns

--- ecis_processing/data_processor.py
+++ ecis_processing/data_processor.py
@@ -19,10 +19,16 @@ class DataProcessor:
         """
         df_new = self.data.copy()
         df_new["data"] = self.data[columns].apply(lambda row: "#".join(row.values.astype(str)), axis=1)
+
+        nat_df = df_new.query("date.isnull()")
+        nat_df = nat_df.groupby("patient_id")["data"].apply("; ".join)
+
         df_new = (df_new.groupby(["patient_id", pd.Grouper(freq="2Y", key="date")])
                   ["data"]
                   .apply("; ".join)
                   .unstack(fill_value=""))
-        df_new.rename(columns=lambda date: f"{date.year}-{date.year + 1}_{suffix}", inplace=True)
+        df_new.rename(columns=lambda date: f"{date.year - 1}-{date.year}_{suffix}", inplace=True)
+
+        df_new[f"unknown_time_{suffix}"] = nat_df
         print(df_new)
         return df_new
